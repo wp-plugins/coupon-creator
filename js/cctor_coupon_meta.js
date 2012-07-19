169cdee5709f633a105101ddb278f8f09ebdc735
brianjessee <brianjessee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-19 17:10:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/coupon-creator/tags/1.20@574757 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/cctor_coupon_meta.js
+++ js/cctor_coupon_meta.js
@@ -1,1 +1,54 @@
-/** * Update color picker element * Used for static & dynamic added elements (when clone) */function cc_update_color_picker(){	var $ = jQuery;	$( '.cc_color-picker' ).each( function()	{		var $this = $( this ),			$input = $this.siblings( 'input.cc_color' );		// Make sure the value is displayed		if ( ! $input.val() )			$input.val( '#' );		$this.farbtastic( $input );	} );}jQuery( document ).ready( function($){	$( '.cc_color' ).focus( function()	{		$( this ).siblings( '.cc_color-picker' ).show();		return false;	} ).blur( function() {		$( this ).siblings( '.cc_color-picker' ).hide();		return false;	} );	cc_update_color_picker();} );		jQuery(function(jQuery) {  			jQuery('.cctor_coupon_image_button').click(function() {  				formfield = jQuery(this).siblings('.cctor_coupon_upload_image');  				preview = jQuery(this).siblings('.cctor_coupon_preview_image');  				tb_show('', 'media-upload.php?post_id=0&type=image&TB_iframe=1');  				window.send_to_editor = function(html) {  					imgurl = jQuery('img',html).attr('src');  					classes = jQuery('img', html).attr('class');  					id = classes.replace(/(.*?)wp-image-/, '');  					formfield.val(id);  					preview.attr('src', imgurl);  					tb_remove();  				}  				return false;  			});  		  			jQuery('.cctor_coupon_clear_image_button').click(function(e){				e.preventDefault();				var defaultImage = jQuery(this).parent().siblings('.cctor_coupon_default_image').text();  				jQuery(this).parent().siblings('.cctor_coupon_upload_image').val('');  				jQuery(this).parent().siblings('.cctor_coupon_preview_image').attr('src', defaultImage);  				return false;  			});  		  		});  
+/**
+ * Update color picker element
+ * Used for static & dynamic added elements (when clone)
+ */
+function cc_update_color_picker()
+{
+	var $ = jQuery;
+	$( '.cc_color-picker' ).each( function()
+	{
+		var $this = $( this ),
+			$input = $this.siblings( 'input.cc_color' );
+		// Make sure the value is displayed
+		if ( ! $input.val() )
+			$input.val( '#' );
+		$this.farbtastic( $input );
+	} );
+}
+jQuery( document ).ready( function($)
+{
+	$( '.cc_color' ).focus( function()
+	{
+		$( this ).siblings( '.cc_color-picker' ).show();
+		return false;
+	} ).blur( function() {
+		$( this ).siblings( '.cc_color-picker' ).hide();
+		return false;
+	} );
+	cc_update_color_picker();
+} );
+		jQuery(function(jQuery) {  
+			jQuery('.cctor_coupon_image_button').click(function() {  
+				formfield = jQuery(this).siblings('.cctor_coupon_upload_image');  
+				preview = jQuery(this).siblings('.cctor_coupon_preview_image');  
+				tb_show('', 'media-upload.php?post_id=0&type=image&TB_iframe=1');  
+				window.send_to_editor = function(html) {  
+					imgurl = jQuery('img',html).attr('src');  
+					classes = jQuery('img', html).attr('class');  
+					id = classes.replace(/(.*?)wp-image-/, '');  
+					formfield.val(id);  
+					preview.attr('src', imgurl);  
+					tb_remove();  
+				}  
+				return false;  
+			});  
+		  
+			jQuery('.cctor_coupon_clear_image_button').click(function(e){
+				e.preventDefault();
+				var defaultImage = jQuery(this).parent().siblings('.cctor_coupon_default_image').text();  
+				jQuery(this).parent().siblings('.cctor_coupon_upload_image').val('');  
+				jQuery(this).parent().siblings('.cctor_coupon_preview_image').attr('src', defaultImage);  
+				return false;  
+			});  
+		  
+		});  
