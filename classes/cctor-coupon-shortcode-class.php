6018c3d17c8d266e6219046af1ce2c10c97531f8
brianjessee <brianjessee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-19 01:26:52 +0000

    Upload remaining Coupon Creator 1.90 to Trunk
    git-svn-id: https://plugins.svn.wordpress.org/coupon-creator/trunk@1028365 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/cctor-coupon-shortcode-class.php
@@ -0,0 +1,123 @@
+<?php
+//If Direct Access Kill the Script
+if( $_SERVER[ 'SCRIPT_FILENAME' ] == __FILE__ )
+	die( 'Access denied.' );
+
+/*
+* Coupon Creator Shortcode Class
+* @version 1.90
+*/
+class Coupon_Creator_Shortcode {
+
+	/*
+	* Register Coupon Creator Shortcode
+	* @version 1.90
+	*/
+	public static function cctor_allcoupons_shortcode($atts) {
+	   //Load Stylesheet for Coupon Creator when Shortcode Called
+	   if( !wp_style_is( 'coupon_creator_css' ) ) {
+		 wp_enqueue_style('coupon_creator_css');
+	   }	 
+	   
+	   //Coupon ID is the Custom Post ID
+	   $cctor_atts = shortcode_atts(array(
+		"totalcoupons" => '-1',
+		"couponid" => '',
+		"coupon_align" => 'cctor_alignnone',
+		"couponorderby" => 'date',
+		"category" => '',
+		"filterid" => ''
+		), $atts, 'coupon' );
+		
+		$filterid = '';
+		$coupon_align = '';
+		
+		$filterid = esc_attr($cctor_atts['filterid']);		
+		$coupon_align = esc_attr($cctor_atts['coupon_align']);
+		
+		// Setup Query for Either Single Coupon or a Loop
+		$cctor_args = array(
+			'p' => esc_attr($cctor_atts['couponid']),
+			'posts_per_page' =>  esc_attr($cctor_atts['totalcoupons']),
+			'cctor_coupon_category' =>  esc_attr($cctor_atts['category']),
+			'post_type' => 'cctor_coupon',
+			'post_status' => 'publish',
+			'orderby' =>  esc_attr($cctor_atts['couponorderby'])
+		);
+				
+		//Filter for all Shortcodes
+		if(has_filter('cctor_shortcode_query_args')) {
+			$cctor_args = apply_filters( 'cctor_shortcode_query_args', $cctor_args );
+		}
+		
+		//Custom Filter ID Set in Shortcode
+		if ($filterid) {
+			if(has_filter('cctor_shortcode_query_args_'.$filterid)) {
+				$cctor_args = apply_filters( 'cctor_shortcode_query_args_'.$filterid, $cctor_args );
+			}		
+		}
+		
+		$coupons = new WP_Query($cctor_args);
+		
+		ob_start();
+		
+		do_action( 'cctor_before_coupon' ); 
+			
+		// The Coupon Loop
+		while ($coupons->have_posts()) {
+
+			$coupons->the_post();
+						
+			$coupon_id = $coupons->post->ID;
+	
+				//Check to show the Coupon
+				if (cctor_expiration_check($coupon_id)) {
+					
+					$outer_coupon_wrap  = apply_filters( 'cctor_outer_content_wrap' , $coupon_id , $coupon_align ); 
+							
+					echo $outer_coupon_wrap['start_wrap'];					
+				
+						//Return If Not Passed Expiration Date
+						$couponimage = apply_filters( 'cctor_image_url' , $coupon_id  );
+						
+						if ($couponimage) {
+						
+							do_action( 'cctor_img_coupon' , $coupon_id , $couponimage ); 
+						
+						} else { 
+						
+							$inner_coupon_wrap  = apply_filters( 'cctor_inner_content_wrap' , $coupon_id  ); 
+
+							echo  $inner_coupon_wrap['start_wrap'];
+							
+								do_action( 'cctor_coupon_deal' , $coupon_id ); 
+								
+								do_action( 'cctor_coupon_terms' , $coupon_id ); 
+								
+								do_action( 'cctor_coupon_expiration' , $coupon_id ); 
+							
+							echo $inner_coupon_wrap['end_wrap'];
+						
+						}
+					
+						do_action( 'cctor_coupon_link' , $coupon_id ); 
+					
+					echo $outer_coupon_wrap['end_wrap'];	
+					
+				} else {
+					//No Coupon Will Show So Print HTML Comment
+					do_action( 'cctor_no_show_coupon' , $coupon_id );
+				}
+			} //End While
+			
+			do_action( 'cctor_after_coupon' ); 
+			
+			/* Restore original Post Data */
+			wp_reset_postdata();
+
+			// Return Variables
+			return ob_get_clean();	 
+
+	} 
+	
+}
---
+++ public/template-functions/cctor-function-wraps.php
@@ -0,0 +1,123 @@
+<?php
+//If Direct Access Kill the Script
+if( $_SERVER[ 'SCRIPT_FILENAME' ] == __FILE__ )
+	die( 'Access denied.' );
+
+/*
+* Return Coupon Categories for Class
+* @version 1.90
+*/
+function cctor_return_coupon_categories($coupon_id) {
+ 	
+	$coupon_terms = get_the_terms( $coupon_id, 'cctor_coupon_category' );
+	
+	if ( $coupon_terms && ! is_wp_error( $coupon_terms ) ) { 
+
+		$cctor_coupon_category_terms = array();
+
+		foreach ( $coupon_terms as $coupon_term ) {
+			$cctor_coupon_category_terms[] = $coupon_term->slug;
+		}
+						
+		$coupon_cat_class = join( " ", $cctor_coupon_category_terms );
+		
+		return $coupon_cat_class;
+	}
+}
+/*
+* Coupon Creator Outer Wrap
+* @version 1.90
+*/
+function cctor_return_outer_coupon_wrap($coupon_id, $coupon_align) { 
+	
+	
+	if(cctor_return_image_url($coupon_id)) {
+		$coupon_img_class = 'cctor-image';
+	} else {
+		$coupon_img_class = '';
+	}
+	
+	$coupon_cat_class = cctor_return_coupon_categories($coupon_id);
+	
+	$outer_coupon_wrap = array();
+	$outer_coupon_wrap['start_wrap'] = '<!--start coupon container here -->
+		<div id="coupon_creator_'. esc_attr($coupon_id).'" class="type-cctor_coupon cctor_coupon_container '.esc_attr($coupon_cat_class).' '.esc_attr($coupon_align).' '.esc_attr($coupon_img_class).'">';
+	
+	$outer_coupon_wrap['end_wrap'] = '</div> <!--end #cctor_coupon_container -->';
+							
+	return $outer_coupon_wrap;
+}			
+/*
+* Coupon Creator Inner Wrap
+* @version 1.90
+*/
+function cctor_return_inner_coupon_wrap($coupon_id) { 
+
+	$coupon_inner_content_wrap = array();
+	
+	$endlink = '';
+	
+	//Build Click to Print Link For Coupon - First Check if Option to Hide is Checked
+	if (cctor_options('cctor_hide_print_link') == 0) {
+		
+		if (cctor_options('cctor_nofollow_print_link') == 1) {
+			$nofollow = "rel='nofollow'";
+		} 
+			
+		$coupon_inner_content_wrap['start_wrap'] = '<a class="cctor_wrap_link" '.$nofollow.' href="'. esc_url(get_permalink($coupon_id)).'" onclick="window.open(this.href);return false;">
+			<div class="cctor_coupon">
+			<div class="cctor_coupon_content" style="border-color:'. esc_attr(get_post_meta($coupon_id, 'cctor_bordercolor', true)).'">';
+	
+		$endlink = '</a>';
+
+	 } else {
+			$coupon_inner_content_wrap['start_wrap'] = '<div class="cctor_coupon">
+			<div class="cctor_coupon_content" style="border-color:'. esc_attr(get_post_meta($coupon_id, 'cctor_bordercolor', true)).'">';
+	
+	 }
+
+	$coupon_inner_content_wrap['end_wrap'] = '</div> <!--end .cctor_coupon_content -->
+												</div> <!--end .cctor_coupon -->'. $endlink;
+	 
+	return $coupon_inner_content_wrap;
+}	
+
+/*
+* Coupon Creator Print Outer Wrap
+* @version 1.90
+*/
+function cctor_return_print_outer_coupon_wrap($coupon_id) { 
+
+	if(cctor_return_image_url($coupon_id)) {
+		$coupon_img_class = ' cctor-image ';
+	} else {
+		$coupon_img_class = '';
+	}
+	$coupon_cat_class = cctor_return_coupon_categories($coupon_id);
+	
+	$outer_coupon_wrap = array();
+	$outer_coupon_wrap['start_wrap'] = '<!--start coupon container -->
+		<div id="coupon_creator_'. esc_attr($coupon_id).'" class="cctor_coupon_container '.esc_attr($coupon_cat_class).' '.esc_attr($coupon_img_class).'">';
+	
+	$outer_coupon_wrap['end_wrap'] = '</div> <!--end #cctor_coupon_container -->';
+							
+	return $outer_coupon_wrap;
+}	
+
+/*
+*  Coupon Creator Print Inner Wrap
+*  @version 1.90
+*/
+function cctor_return_print_inner_coupon_wrap($coupon_id) { 
+	
+	$coupon_inner_content_wrap = array();
+	$coupon_inner_content_wrap['start_wrap'] = '<div class="cctor_coupon">
+	<div class="cctor_coupon_content" style="border-color:'. esc_attr(get_post_meta($coupon_id, 'cctor_bordercolor', true)).'">';
+	
+	$coupon_inner_content_wrap['end_wrap'] = '</div> <!--end .cctor_coupon_content -->
+							</div> <!--end .cctor_coupon -->';
+							
+	return $coupon_inner_content_wrap;
+}	
+
+
